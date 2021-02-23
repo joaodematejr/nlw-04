492b56417e0acfc925765e2de43c33d39eb2d72c
João Dematé Jr <joaodematejr@gmail.com>
2021-02-23 00:06:15 -0300

    Implementação teste automatizados e criação de novas rotas e controles

--- rocketpay/lib/rocketpay/numbers.ex
+++ rocketpay/lib/rocketpay/numbers.ex
@@ -2,17 +2,17 @@ defmodule Rocketpay.Numbers do
   def sum_from_file(filename) do
     "#{filename}.csv"
     |> File.read()
-    |> handle_file()
+    |> handle_file
   end
 
   defp handle_file({:ok, result}) do
     result =
       result
-    |> String.split(",")
-    |> Enum.map(fn number -> String.to_integer(number) end)
-    |> Enum.sum()
-
-  {:ok, %{result: result}}
+      |> String.split(",")
+      |> Stream.map(fn number -> String.to_integer(number) end)
+      |> Enum.sum()
+    {:ok, %{result: result}}
   end
-  defp handle_file({:error, reason}), do: {:error, "Invalid File!"}
+
+  defp handle_file({:error, _reason}), do: {:error, %{message: "Invalid file!"}}
 end
